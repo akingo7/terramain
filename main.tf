0ed5ae1bbcfb31be91f10daf329c6f0912950715
akingo <akinmoyerogabriel@gmail.com>
2022-04-06 17:26:16 +0100

    Changes

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 
 module "s3-backend-app" {
   source  = "app.terraform.io/gabrieldevops/s3-backend-app/aws"
