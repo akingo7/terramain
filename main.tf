73c51cb967684295daae193963cde593e1ae245c
akingo7 <akinmoyerogabriel@gmail.com>
2022-04-23 02:53:10 -0700

    Updates

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
-
 module "s3-backend-app" {
-  source  = "app.terraform.io/gabrieldevops/s3-backend-app/aws"
-  version = "1.0.0"
-  region  = var.region
+  source      = "app.terraform.io/gabrieldevops/s3-backend-app/aws"
+  version     = "1.0.0"
+  region      = var.region
   dev_prefix  = var.dev_prefix
   prod_prefix = var.prod_prefix
 }
